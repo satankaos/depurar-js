5d34c96629d0e5099e9fa624fda48a4087bedeae
gamiz <71780447+satankaos@users.noreply.github.com>
2022-10-19 19:26:36 +0200

    add

---
+++ script.js
@@ -0,0 +1,13 @@
+// setTimeout(()=>{
+// var tit=document.getElementById("pregunta");
+// tit.innerText = "se solo feo"
+// tit.style.backgroundColor="blue" 
+// tit.style.color="white" 
+
+// },6000)
+
+    // var subVentana=window.open("https://www.google.es","nueva","height=800,width=600");
+    // setTimeout(() => {
+    //     subVentana.close
+    // }, 6000);
+
